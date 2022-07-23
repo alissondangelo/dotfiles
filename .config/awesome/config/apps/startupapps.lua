cf0d7e9f352f33e05bde6dc3089068ed20f0a2d5
alissondangelo <alisson73development@gmail.com>
2022-07-23 03:58:43 -0300

    formatation

--- .config/awesome/config/apps/startupapps.lua
+++ .config/awesome/config/apps/startupapps.lua
@@ -3,23 +3,20 @@
 local awful = require('awful')
 
 local apps = {
-    "gwe", --"gwe --hide-window",
+    "gwe", -- "gwe --hide-window",
     "feh --bg-scale $(< ${HOME}/.cache/wal/wal)",
     "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1",
     "picom -b --experimental-backends --backend glx --config  $HOME/.config/picom/picom.conf",
     "numlockx on"
 }
 
-
 local function run_once(cmd)
-  local findme = cmd
-  local firstspace = cmd:find(' ')
-  if firstspace then
-    findme = cmd:sub(0, firstspace - 1)
-  end
-  awful.spawn.with_shell(string.format('pgrep -u $USER -x %s > /dev/null || (%s)', findme, cmd))
+    local findme = cmd
+    local firstspace = cmd:find(' ')
+    if firstspace then findme = cmd:sub(0, firstspace - 1) end
+    awful.spawn.with_shell(string.format(
+                               'pgrep -u $USER -x %s > /dev/null || (%s)',
+                               findme, cmd))
 end
 
-for _, app in ipairs(apps) do
-  run_once(app)
-end
+for _, app in ipairs(apps) do run_once(app) end
