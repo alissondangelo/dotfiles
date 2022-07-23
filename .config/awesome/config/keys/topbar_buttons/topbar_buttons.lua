ad14d5f938ceb67ea573393b0ee044584366ad8c
alissondangelo <alisson73development@gmail.com>
2022-07-22 21:39:04 -0300

    changed topbar layout box buttons

--- .config/awesome/config/keys/topbar_buttons/topbar_buttons.lua
+++ .config/awesome/config/keys/topbar_buttons/topbar_buttons.lua
@@ -46,6 +46,13 @@ local topbar_buttons = {
             awful.client.focus.byidx(-1)
         end)
     ),
+
+    layoutbox = gears.table.join(
+        awful.button({ }, 1, function () awful.layout.inc( 1) end),
+        awful.button({ }, 3, function () awful.layout.inc(-1) end),
+        awful.button({ }, 4, function () awful.layout.inc( 1) end),
+        awful.button({ }, 5, function () awful.layout.inc(-1) end)
+    ),
 }
 
 return topbar_buttons
