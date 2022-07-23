572b211b6daa8348baf3778000f95014b62b425c
alissondangelo <alisson73development@gmail.com>
2022-07-22 22:55:26 -0300

    optimizations

--- .local/bin/change_wallpaper_theme.sh
+++ .local/bin/change_wallpaper_theme.sh
@@ -4,6 +4,6 @@ WALLPAPERDIR=$HOME/Pictures/Wallpapers
 
 cd $WALLPAPERDIR &&
 FILENAME=$(yad --title="Select Wallpaper" --file --add-preview --large-preview) &&
-alacritty -e chameleon.py -i $FILENAME
+alacritty --class floating -e chameleon.py -i $FILENAME
 #$HOME/.local/bin/starttree.py
 echo "awesome.restart()" | awesome-client
