7d7d14c848ac84e9db14cbc502c6f304bdfb12d7
alissondangelo <alisson73development@gmail.com>
2022-07-22 23:48:41 -0300

    some changes in menu

--- .config/awesome/config/menu/menu.lua
+++ .config/awesome/config/menu/menu.lua
@@ -1,12 +1,12 @@
 local awful = require("awful")
 
 awful.util.mymainmenu = awful.menu({ items = {
-         { "Log out", function() awesome.quit() end },
-         { "Sleep", "systemctl suspend" },
-         { "Restart", "reboot" },
-         { "Power Off", "shutdown now" },
-         -- other triads can be put here
-     }
- })
- -- Menubar configuration
+    { "🫡 Log out", function() awesome.quit() end },
+    { "🤐 Lock", function() awful.spawn.with_shell("xset dpms force off && slock")end},
+    { "😴 Sleep", "systemctl suspend" },
+    { "🙃 Restart", "reboot" },
+    { "😔 Power Off", "shutdown now" }, 
+    -- other triads can be put here
+}})
+ -- Hide on mouse leave
 awful.util.mymainmenu.wibox:connect_signal("mouse::leave", function() awful.util.mymainmenu:hide() end)
