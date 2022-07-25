b1aa899f4d77644e60cebbed81715a22b8d4c855
alissondangelo <alisson73development@gmail.com>
2022-07-25 11:36:51 -0300

    color changes

--- .config/awesome/config/theme/xresources.lua
+++ .config/awesome/config/theme/xresources.lua
@@ -15,34 +15,36 @@ local theme = dofile(themes_path.."default/theme.lua")
 
 local icons = os.getenv("HOME") .. "/.config/awesome/config/theme/icons/layout/"
 
+--font------------------------------------------------------------------------------
 local font = "Roboto Mono"
 local font_size = "12"
 theme.font          = font .. " Light " .. font_size
 local font_bold     = font .. " Bold " .. font_size
 
+--bg_colors-------------------------------------------------------------------------
 theme.bg_normal     = xrdb.background .. "88"
 theme.bg_focus      = xrdb.color4
-theme.bg_urgent     = xrdb.color5
-theme.bg_minimize   = xrdb.color8 .. "22"
-theme.bg_systray    = theme.bg_normal
-
-theme.fg_normal     = xrdb.foreground
+theme.bg_urgent     = "#AA0000"
+theme.bg_minimize   = xrdb.color8 .. "44"
+theme.bg_systray    = theme.bg_focus
+--fg_colors-------------------------------------------------------------------------
+theme.fg_normal     = xrdb.color8
 theme.fg_focus      = xrdb.color7
-theme.fg_urgent     = theme.bg_normal
-theme.fg_minimize   = theme.bg_normal
-
+theme.fg_urgent     = xrdb.background
+theme.fg_minimize   = xrdb.background
+--border_colors---------------------------------------------------------------------
 theme.border_normal = xrdb.color0
-theme.border_focus  = theme.bg_focus
+theme.border_focus  = xrdb.color6
 theme.border_marked = xrdb.color10
 
+--gaps and border width-------------------------------------------------------------
 theme.useless_gap   = dpi(5)
-
-theme.border_width  = dpi(3)
+theme.border_width  = dpi(2)
 theme.border_radius = dpi(3)
-theme.tooltip_border_width = dpi(3)
+theme.tooltip_border_width = dpi(2)
+local tasklist_border_width = 1
 
-theme.bg_systray    = theme.bg_focus
-
+--tooltip colors--------------------------------------------------------------------
 theme.tooltip_fg = theme.fg_normal
 theme.tooltip_bg = theme.bg_normal
 theme.tooltip_border_color = theme.border_focus
@@ -59,9 +61,9 @@ end
 --shape
 theme.taglist_shape = rounded_rect_shape
 --color
-theme.taglist_bg_occupied = xrdb.color3 .. "88"
+theme.taglist_bg_occupied = xrdb.color3 .. "66"
 theme.taglist_bg_empty = "#00000000"
-theme.taglist_bg_focus = theme.border_focus
+theme.taglist_bg_focus = theme.border_focus .. "AA"
 --disable squares
 theme.taglist_squares_sel = nil
 theme.taglist_squares_unsel = nil
@@ -73,22 +75,21 @@ theme.tasklist_bg_normal = theme.bg_focus .. "22"
 theme.tasklist_shape_border_color = theme.bg_focus .. "44"
 --focus color
 theme.tasklist_fg_focus = theme.fg_focus
-theme.tasklist_bg_focus = theme.border_focus .. "55"
+theme.tasklist_bg_focus = theme.border_focus .. "44"
 theme.tasklist_shape_border_color_focus = theme.border_focus
 theme.tasklist_font_focus = font_bold
 --minimized
-theme.tasklist_shape_border_color_minimized = xrdb.color8 .. "22"
+theme.tasklist_shape_border_color_minimized = xrdb.color8 .. "33"
 --shape
 theme.tasklist_shape = rounded_rect_shape
 theme.tasklist_shape_focus = rounded_rect_shape
 theme.tasklist_shape_minimized = rounded_rect_shape
 --border width
-local border_width = 2
-theme.tasklist_shape_border_width = border_width
-theme.tasklist_shape_border_width_focus = border_width
-theme.tasklist_shape_border_width_minimized = border_width
+theme.tasklist_shape_border_width = tasklist_border_width
+theme.tasklist_shape_border_width_focus = tasklist_border_width
+theme.tasklist_shape_border_width_minimized = tasklist_border_width
 --spacing
-theme.tasklist_spacing = border_width
+theme.tasklist_spacing = tasklist_border_width
 --widget
 theme.tasklist_widget = wibox.widget
 --template
