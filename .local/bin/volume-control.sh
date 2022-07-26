33a63c2cd192c20ec7acf22f428f4fc01fb4a1c5
alissondangelo <alisson73development@gmail.com>
2022-07-26 18:56:29 -0300

    changed all to amixer

--- .local/bin/volume-control.sh
+++ .local/bin/volume-control.sh
@@ -3,18 +3,16 @@
 
 if [[ "$1" = "up" ]]; then
     amixer set Master 1%+
-    VOL="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
-    notify-send "${VOL}%" -r 1 --urgency low
+    notify-send "$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')%" -r 1 --urgency low
 elif [[ "$1" = "down" ]]; then
     amixer set Master 1%-
-    VOL="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
-    notify-send "${VOL}%" -r 1 --urgency low
+    notify-send "$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')%" -r 1 --urgency low
 elif [[ "$1" = "toggle" ]]; then
-    pactl set-sink-mute @DEFAULT_SINK@ toggle
-    notify-send 'Volume toggle.' -r 1 --urgency low
+    amixer set Master toggle
+    notify-send "$(amixer get Master | tail -1 | grep -c '\[on\]')" -r 1 --urgency low
 elif [[ "$1" = "mic_toggle" ]]; then
-    pactl set-source-mute @DEFAULT_SOURCE@ toggle
-    notify-send $(pactl get-source-mute @DEFAULT_SOURCE@) -r 1 --urgency low
+    amixer set Capture toggle
+    notify-send $(amixer get Capture | tail -1 | grep -c '\[on\]') -r 1 --urgency low
 else
     echo "No argument."
 fi
