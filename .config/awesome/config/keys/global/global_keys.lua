f462162f9db90cf63c996628b63e918c3f8f6757
alissondangelo <alisson73development@gmail.com>
2022-07-27 16:37:45 -0300

    added dynamic layout keys

--- .config/awesome/config/keys/global/global_keys.lua
+++ .config/awesome/config/keys/global/global_keys.lua
@@ -73,6 +73,14 @@ local globalkeys = mytable.join(
         {description = "select next", group = "layout"}),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(-1) end,
         {description = "select previous", group = "layout"}),
+    awful.key({ modkey, altkey    }, "h",     function () awful.tag.incnmaster( 1, nil, true) end,
+        {description = "increase the number of master clients", group = "layout"}),
+    awful.key({ modkey, altkey    }, "l",     function () awful.tag.incnmaster(-1, nil, true) end,
+        {description = "decrease the number of master clients", group = "layout"}),
+    awful.key({ modkey, altkey    }, "j",     function () awful.tag.incncol( 1, nil, true)    end,
+        {description = "increase the number of columns", group = "layout"}),
+    awful.key({ modkey, altkey    }, "k",     function () awful.tag.incncol(-1, nil, true)    end,
+        {description = "decrease the number of columns", group = "layout"}),
 
     -- tag keys ---------------------------------------------------------------------
     awful.key({ modkey, altkey    }, "Left",   awful.tag.viewprev,
