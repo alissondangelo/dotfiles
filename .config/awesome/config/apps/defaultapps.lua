fb88eaf0a0d508f2b87103434c1ef620cc8f5112
alissondangelo <alisson73development@gmail.com>
2025-07-07 08:02:42 -0300

    changed from pcmanfm to pcmanfm-qt

--- .config/awesome/config/apps/defaultapps.lua
+++ .config/awesome/config/apps/defaultapps.lua
@@ -1,7 +1,7 @@
 terminal    = "alacritty"
 editor      = os.getenv("EDITOR") or "nano"
 editor_cmd  = terminal .. " -e " .. editor
-fileManager = "pcmanfm"
+fileManager = "pcmanfm-qt"
 browser1    = "brave"
 browser2    = "firefox"
 browser3    = "qutebrowser"
